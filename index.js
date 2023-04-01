0732344460e9f70d4c3139b96a103ea2729274ca
UlfetZakirli <ulfetzakirli@gmail.com>
2023-04-01 23:59:31 +0400

    feat: Image upload router

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 require('dotenv').config()
 require('colors')
 const connection = require('./config/db.js')
+const Upload = require('./routes/upload.js')
 
 const app = express()
 connection()
+
+app.use('/file', Upload)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port:http://localhost:${PORT}`.blue.bold))
